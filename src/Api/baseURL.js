058831395708702139315867172c4aa03f77432b
zezo <egypte.2200@gmail.com>
2023-08-07 04:33:15 +0300

    update base url

--- src/Api/baseURL.js
+++ src/Api/baseURL.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 // const baseURL = axios.create({baseURL: "http://localhost:8000"})
 // const baseURL = axios.create({baseURL: "https://long-cyan-cheetah-hem.cyclic.app"})
-const baseURL = axios.create({baseURL: "e-commercenodejs-production.up.railway.app"})
+const baseURL = axios.create({baseURL: "https://e-commercenodejs-production.up.railway.app"})
 
 export default baseURL
 
