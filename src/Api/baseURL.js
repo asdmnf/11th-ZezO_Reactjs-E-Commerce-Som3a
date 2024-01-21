076e367b501f7f2eb5bc6df57050720c365b71d3
zezo <egypte.2200@gmail.com>
2024-01-21 23:25:39 +0200

    update backend server URL

--- src/Api/baseURL.js
+++ src/Api/baseURL.js
@@ -3,7 +3,8 @@ import axios from "axios"
 
 // const baseURL = axios.create({baseURL: "http://localhost:8000"})
 // const baseURL = axios.create({baseURL: "https://long-cyan-cheetah-hem.cyclic.app"})
-const baseURL = axios.create({baseURL: "https://e-commercenodejs-production.up.railway.app"})
+// const baseURL = axios.create({baseURL: "https://e-commercenodejs-production.up.railway.app"})
+const baseURL = axios.create({baseURL: "https://nodejs-e-commerce-0soe.onrender.com"})
 
 export default baseURL
 
